55b9fe23f39280b3549f1ab70c8730963f090341
Matias Nahuel Morata <nahuelmorata@gmail.com>
2024-04-10 23:09:34 -0300

    feat: cambio el formato del year por el release date basado en la precision

--- app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
+++ app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
@@ -13,7 +13,39 @@ sealed class Song {
         var isLocallyStored: Boolean = false
     ) : Song() {
 
-        val year: String = releaseDate.split("-").first()
+        fun releaseDate(): String {
+            return when(releaseDatePrecision) {
+                "year" -> "$releaseDate (${if (isLeapYear(releaseDate.toInt())) "" else "not "}a leap year)"
+                "month" -> {
+                    val splitReleaseDate = releaseDate.split('-')
+                    "${monthToText(splitReleaseDate[1].toInt())}, ${splitReleaseDate[0]}"
+                }
+                "day" -> {
+                    val splitReleaseDate = releaseDate.split('-')
+                    "${splitReleaseDate[2]}/${splitReleaseDate[1]}/${splitReleaseDate[0]}"
+                }
+                else -> releaseDate
+            }
+        }
+
+        private fun isLeapYear(year: Int): Boolean =
+            (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+
+        private fun monthToText(month: Int): String = when(month) {
+            1 -> "January"
+            2 -> "February"
+            3 -> "March"
+            4 -> "April"
+            5 -> "May"
+            6 -> "June"
+            7 -> "July"
+            8 -> "August"
+            9 -> "September"
+            10 -> "October"
+            11 -> "November"
+            12 -> "December"
+            else -> ""
+        }
     }
 
     object EmptySong : Song()
