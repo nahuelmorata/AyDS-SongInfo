eb5a8591cb6744024a244de44f8cd7aa08b5f612
Nahuel Morata <nahuelmorata@gmail.com>
2024-05-25 01:10:49 -0300

    test: agrego tests del presentador de other info

--- app/src/test/java/ayds/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
+++ app/src/test/java/ayds/songinfo/moredetails/presentation/OtherInfoPresenterTest.kt
@@ -1,4 +1,34 @@
 package ayds.songinfo.moredetails.presentation
 
+import ayds.songinfo.moredetails.domain.Article
+import ayds.songinfo.moredetails.domain.ArticleRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert
+import org.junit.Test
+
 class OtherInfoPresenterTest {
+    private val articleRepository: ArticleRepository = mockk()
+    private val otherInfoDescriptionHelper: OtherInfoDescriptionHelper = OtherInfoDescriptionHelperImpl()
+
+    private val otherInfoDescriptionPresenter = OtherInfoPresenterImpl(
+        articleRepository, otherInfoDescriptionHelper
+    )
+
+    @Test
+    fun `get artist by name should notify by observable`() {
+        val article = Article.ArtistBiography(
+            "name",
+            "biography",
+            "article",
+            false,
+        )
+        every { articleRepository.getArticleByArtistName("name") } returns article
+
+        otherInfoDescriptionPresenter.getArtistBiography("name")
+        otherInfoDescriptionPresenter.otherInfoObservable.subscribe { state ->
+            Assert.assertEquals("name", state.artistName)
+            Assert.assertEquals("article", state.artistUrl)
+        }
+    }
 }
